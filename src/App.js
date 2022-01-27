d4d3c9fcc9ae162c605ca422856800dabd7d3d83
hieund20 <hieund898@gmail.com>
2022-01-27 11:29:34 +0700

    build header navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.scss';
-import Home from './components/pages/Home/Home';
+import React, { Suspense } from "react";
+import Home from "./components/pages/Home/Home";
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./components/pages/NotFound/NotFound";
+import Header from "./components/layouts/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/addPost" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/layouts/Header/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Nav,
+  Navbar,
+  NavLink,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  UncontrolledDropdown,
+} from "reactstrap";
+import logo from "../../../assets/icons/logo.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Navbar color="light" expand="md" light>
+        <NavbarBrand>
+          <Link to={"/"}>
+            <img
+              src={logo}
+              style={{ width: "100%", height: "50px" }}
+              alt="logo"
+            />
+          </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to={"/"}>Home</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to={"/addPost"}>Add New Post</Link>
+              </NavLink>
+            </NavItem>
+            {/* <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
+        </Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
