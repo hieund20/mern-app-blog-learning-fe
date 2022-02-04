bc001e1810400531987341b07924063578020be3
hieund20 <hieund898@gmail.com>
2022-02-04 21:50:31 +0700

    comment WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React, { Suspense } from "react";
-import Home from "./components/pages/Home/Home";
-import "./App.scss";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import React, { Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NotFound from "./components/pages/NotFound/NotFound";
+import "./App.scss";
 import Header from "./components/layouts/Header/Header";
 import AddPost from "./components/pages/AddPost/AddPost";
-import PostDetail from "./components/pages/PostDetail/PostDetail";
 import EditPost from "./components/pages/EditPost/EditPost";
+import Home from "./components/pages/Home/Home";
+import NotFound from "./components/pages/NotFound/NotFound";
+import PostDetail from "./components/pages/PostDetail/PostDetail";
+
+//Lazy loading components
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyDelFe4ZXH1u06vTWbJka8J-k8pMWVR0BE",
+  authDomain: "mern-blog-28420.firebaseapp.com",
+};
+firebase.initializeApp(config);
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  //Handle firebase auth changed
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (!user) {
+          console.log("User is not logged");
+          return;
+        }
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver();
+  }, []);
+
   return (
     <div className="App">
       <Suspense fallback={<div>Loading...</div>}>
@@ -16,7 +43,10 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route
+              path="/posts/:id"
+              element={<PostDetail isSignedIn={isSignedIn} />}
+            />
             <Route path="/posts/addPost" element={<AddPost />} />
             <Route path="/posts/editPost/:id" element={<EditPost />} />
             <Route path="*" element={<NotFound />} />
--- src/components/pages/PostDetail/PostDetail.js
+++ src/components/pages/PostDetail/PostDetail.js
@@ -1,14 +1,34 @@
-import React, { useEffect } from "react";
+import { Avatar, TextField } from "@mui/material";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import _ from "lodash";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPostDetail } from "../../../store/actions/postsAction";
 import ContentLayout from "../../layouts/Content/Content";
-import _ from "lodash";
-import moment from "moment";
-
 import "./PostDetail.scss";
 
-const PostDetail = () => {
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+const PostDetail = (props) => {
+  const { isSignedIn } = props;
+  const [userLogged, setUserLogged] = useState(null);
+
+  console.log("is signed", isSignedIn);
+
   const dispatch = useDispatch();
   const postDetail = useSelector((state) => state.postDetail);
   const { id } = useParams();
@@ -22,9 +42,20 @@ const PostDetail = () => {
 
   console.log(postDetail);
 
+  //If user log out, it will return null
+  console.log("user logged", firebase.auth().currentUser);
+
   useEffect(() => {
     fetchPostDetail();
   }, []);
+
+  useEffect(() => {
+    if (!isSignedIn) return;
+
+    setUserLogged(firebase?.auth()?.currentUser?.multiFactor?.user);
+  }, []);
+
+  console.log("user logged", userLogged);
 
   return (
     <ContentLayout>
@@ -42,6 +73,36 @@ const PostDetail = () => {
               <img src={postDetail?.data?.thumbnailImage} alt="thumbnail" />
             </div>
             <p>{postDetail?.data?.content}</p>
+            <div className="post-detail-container__comment">
+              <p>Bình luận</p>
+              {!isSignedIn ? (
+                <div className="post-detail-container__comment--logout">
+                  <p>Đăng nhập để bình luận</p>
+                  <StyledFirebaseAuth
+                    uiConfig={uiConfig}
+                    firebaseAuth={firebase.auth()}
+                  />
+                </div>
+              ) : (
+                <div className="post-detail-container__comment--logged">
+                  <div>
+                    <Avatar
+                      alt={userLogged?.displayName}
+                      src={userLogged?.photoURL}
+                      sx={{ marginRight: "8px" }}
+                    />
+                    <div>{userLogged?.displayName}</div>
+                  </div>
+                  <form>
+                    <textarea type="text" />
+                    <div>
+                      <input type="submit" value="Gửi bình luận" />
+                    </div>
+                  </form>
+                  <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
